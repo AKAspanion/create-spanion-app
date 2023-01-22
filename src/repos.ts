cc4c32ef735ccb1f2cf47060998fd7de59d58c41
AKASpanion <panditankit1995@gmail.com>
2023-01-22 19:36:19 +0530

    Added svelte repo

--- src/repos.ts
+++ src/repos.ts
@@ -3,4 +3,5 @@ export default {
   tanstack: "https://github.com/AKAspanion/tanstack-template",
   frontend: "https://github.com/AKAspanion/frontend-template",
   backend: "https://github.com/AKAspanion/backend-template",
+  svelte: "https://github.com/AKAspanion/svelte-template",
 };
