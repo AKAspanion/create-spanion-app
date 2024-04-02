12c32aaa4586c839ee0453baf6575b4b297a2e93
AKASpanion <panditankit1995@gmail.com>
2024-04-02 17:33:36 +0530

    Added nextjs template in list

--- src/repos.ts
+++ src/repos.ts
@@ -4,4 +4,5 @@ export default {
   frontend: "https://github.com/AKAspanion/frontend-template",
   backend: "https://github.com/AKAspanion/backend-template",
   svelte: "https://github.com/AKAspanion/svelte-template",
+  nextjs: "https://github.com/AKAspanion/nextjs-template",
 };
